e62cb33daf8eaddfcdbb98205bb7d8bb2901b98f
1mb33 <matt.ib.g.12@gmail.com>
2026-07-28 17:37:39 +0200

    refact: dedicated function for converting degrees

--- Engine/Math/Public/Consts.hpp
+++ Engine/Math/Public/Consts.hpp
@@ -4,8 +4,13 @@
 namespace B33::Math
 {
 
-constexpr const float B33_DEG_TO_RAD = 0.01745329f;
-constexpr const float B33_PI_RAD     = 180.f * 0.01745329f;
+constexpr float B33_DEG_TO_RAD_RATIO = 0.01745329f;
+constexpr float B33_PI_RAD_RATIO     = 180.f * 0.01745329f;
+
+constexpr float B33_DEG_TO_RAD( float deg )
+{
+    return B33_DEG_TO_RAD_RATIO * deg;
+}
 
 } // namespace B33::Math
 #endif // !B33_MATH_CONSTS_H
