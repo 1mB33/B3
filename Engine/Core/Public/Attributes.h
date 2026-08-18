1036cf3a9d651afdc54d6ee9e092909d31452ac8
1mB33 <matt.ib.g.12@gmail.com>
2026-08-18 21:19:12 +0200

    feat: warn unused attribute

--- Engine/Core/Public/Attributes.h
+++ Engine/Core/Public/Attributes.h
@@ -1,3 +1,6 @@
+#ifndef B33_ATTRIBUTES_H
+#define B33_ATTRIBUTES_H
+
 #if defined( _GNU )
 #    define __B33_ATTRIBUTE_MIGHT_BE_UNUSED __attribute__( ( unused ) )
 #elif defined( _MSVC )
@@ -6,4 +9,16 @@
 #    define __B33_ATTRIBUTE_MIGHT_BE_UNUSED __attribute__( ( unused ) )
 #else
 #    error "Compiler isn't supported"
+#endif
+
+#if defined( _GNU )
+#    define __B33_ATTRIBUTE_WARN_UNUSED __attribute__( ( warn_unused_result ) )
+#elif defined( _MSVC )
+#    define __B33_ATTRIBUTE_WARN_UNUSED _Check_return_
+#elif defined( _CLANG )
+#    define __B33_ATTRIBUTE_WARN_UNUSED __attribute__( ( warn_unused_result ) )
+#else
+#    error "Compiler isn't supported"
+#endif
+
 #endif
