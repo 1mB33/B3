0a7bf6b5141c8c8c4d3cde3c028f53bc407399b0
1mB33 <matt.ib.g.12@gmail.com>
2026-08-24 14:40:14 +0200

    feat: ps script that copies dlls to project output dirs

---
+++ Scripts/Win32/CopyDLLPerProject.ps1
@@ -0,0 +1,17 @@
+param (
+   [Parameter(Mandatory=$true)][string]$root_path,
+   [Parameter(Mandatory=$true)][string]$project_list
+)
+
+$dlls = Get-ChildItem -Path $root_path -Filter *.dll -File
+
+Write-Output $root_path
+Write-Output $project_list
+Write-Output $dlls
+
+$project_list.Split(";") | ForEach {  
+    foreach ($dll in $dlls) 
+    {
+        Copy-Item $dll -Destination $root_path\$_\
+    }
+}
