1c73da8c85a802cee96116dfe8cda813883acb56
1mB33 <matt.ib.g.12@gmail.com>
2026-09-19 16:36:03 +0200

    build: build managment script working implementation with basic features for the linux

--- Build.sh
+++ Build.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/bash
 
 CMD_NOT_FOUND_RESULT=127
-dependencies=("cmake" "python --help")
+dependencies=("cmake" "python --help" "make")
 root_file="./Scripts/Win32/CopyDLLPerProject.ps1"
-flag=False
+is_invalid=False
 
 printHelp()
 {
-    echo "Help:"
+    echo "Help manual"
+    echo ""
+    echo "      command:"
+    echo "          $0 [build-switches] --All"
+    echo "          $0 [build-switches] --Tests"
+    echo "          $0 [build-switches] --Project <project-name>"
+    echo ""
+    echo ""
+    echo "The scripts defaults to debug build type."
 }
 
 for dep in "${dependencies[@]}"
@@ -19,7 +27,7 @@ do
     if [[ "$result" == "$CMD_NOT_FOUND_RESULT" ]]; then
 
         echo "error: Missing dependency '$dep' ($result)"
-        flag=True
+        is_invalid=True
 
     fi
 
@@ -28,7 +36,7 @@ done
 if [[ ! -f "$root_file" ]]; then
 
     echo "error: Please call the script from the root directory of the engine."
-    flag=True
+    is_invalid=True
 
 fi
 
@@ -37,14 +45,21 @@ if [[ "$#" -lt 1 ]]; then
     echo "error: Invalid amount of arguments."
     echo ""
     printHelp
-    flag=True
+    is_invalid=True
     
 fi
 
-if [[ "$flag" == True ]]; then
+if [[ "$is_invalid" == True ]]; then
 
     exit 1
     
 fi
 
 python ./Scripts/BuildManagment.py "$@"
+
+result=$?
+if [[ "$result" != "0" ]]; then
+
+    printHelp
+
+fi
--- Scripts/BuildManagment.py
+++ Scripts/BuildManagment.py
@@ -1,15 +1,85 @@
 import sys
+import os
+
+
+g_IsVerbose=True
+g_RunFlag=0
+g_ProjectBuildName="All"
+g_ProjectBuildDir=""
+g_HasFiles=False
+g_GenerateCmd="cmake ../../"
+
+
+def IsTool(name):
+    from shutil import which
+    return which(name) is not None
+
+
+def VerbosePrint(msg: str):
+    if g_IsVerbose:
+        print("VERBOSE: " + msg)
+    pass
+
+
+F_BUILD_ALL=0x01
+F_BUILD_TESTS=0x02
+def AppendRunFlag(switch: str) -> int:
+    global g_RunFlag
+
+    VerbosePrint("Parsing switch: " + switch)
+
+    match switch.lower():
+        case "--all":
+            g_RunFlag = g_RunFlag | F_BUILD_ALL;
+        case "--tests":
+            g_RunFlag = g_RunFlag | F_BUILD_TESTS;
 
 
 def ValidateSystem():
+    global g_ProjectBuildDir
+    global g_GenerateCmd
+    global g_HasFiles
+    
+    generateCachePath = g_ProjectBuildDir + "CMakeCache.txt"
+    if os.path.exists(generateCachePath):
+        g_HasFiles = True
+        return
+
+    # Prefer ninja over make
+    if IsTool("ninja"):
+        g_GenerateCmd = g_GenerateCmd + " -G Ninja"
     pass
 
 
 def CreateBuildResources():
+    global g_ProjectBuildDir
+
+    g_ProjectBuildDir = "./Build/" + g_ProjectBuildName + "/"
+
     ValidateSystem()
+
+    if not os.path.exists(g_ProjectBuildDir):
+        os.makedirs(g_ProjectBuildDir)
+
+    if not g_HasFiles:
+        os.chdir(g_ProjectBuildDir)
+        generateResult = os.system(g_GenerateCmd)
+        VerbosePrint("generateResult: " + str(generateResult))
+        os.chdir("../..")
+
+    pass
 
 
 if __name__ == "__main__":
-    print("Build managment script called")
-    if (len(sys.argv) <= 1):
-        exit()
+    VerbosePrint("Build script called")
+    for i in range(1, len(sys.argv)):
+        AppendRunFlag(sys.argv[i])
+    VerbosePrint("g_RunFlag: " + hex(g_RunFlag))
+
+    CreateBuildResources()
+
+    os.chdir(g_ProjectBuildDir)
+    if (g_RunFlag & (F_BUILD_TESTS | F_BUILD_ALL)):
+        os.system("cmake --build .")
+    
+    exit(0)
